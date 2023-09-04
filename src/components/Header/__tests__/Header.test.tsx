ea8c754cdcc5e3a0f3560f12ceb2c1e60ea949db
gleison <gleisondeivison3@gmail.com>
2023-09-04 20:12:52 -0300

    testes com react e redux

--- src/components/Header/__tests__/Header.test.tsx
+++ src/components/Header/__tests__/Header.test.tsx
@@ -1,17 +1,42 @@
-import { render, screen } from '@testing-library/react'
-import { Provider } from 'react-redux'
+import { screen } from '@testing-library/react'
 import Header from '..'
 
-import { store } from '../../../store'
+import { renderizaComProvider } from '../../../utils/test'
 
 describe('Teste para o componente header', () => {
   test('Deve renderizar corretamente', () => {
-    const { debug } = render(
-      <Provider store={store}>
-        <Header />
-      </Provider>
-    )
-    debug()
+    renderizaComProvider(<Header />)
     expect(screen.getByText('EBAC Games')).toBeInTheDocument()
+  })
+
+  test('Deve renderizar com 2 itens no carrinho', () => {
+    renderizaComProvider(<Header />, {
+      preloadedState: {
+        carrinho: {
+          itens: [
+            {
+              id: 1,
+              categoria: 'RPG',
+              imagem: '',
+              plataformas: ['Windows'],
+              preco: 150.9,
+              precoAntigo: 199.9,
+              titulo: 'Elder Ring'
+            },
+            {
+              id: 2,
+              categoria: 'RPG',
+              imagem: '',
+              plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+              preco: 199.9,
+              precoAntigo: 299.9,
+              titulo: 'Hogwards Legacy'
+            }
+          ]
+        }
+      }
+    })
+
+    expect(screen.getByTestId('qtd-carrinho').innerHTML).toContain('2 itens')
   })
 })
